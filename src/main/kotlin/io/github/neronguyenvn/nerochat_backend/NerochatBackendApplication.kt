9d22f82491e147dfd01a00d75561918ffe73c1a0
Nero Nguyen <50225603+neronguyenvn@users.noreply.github.com>
2025-11-13 17:16:10 +0700

    initial commit

---
+++ src/main/kotlin/io/github/neronguyenvn/nerochat_backend/NerochatBackendApplication.kt
@@ -0,0 +1,11 @@
+package io.github.neronguyenvn.nerochat_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NerochatBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<NerochatBackendApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/neronguyenvn/nerochat_backend/NerochatBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.neronguyenvn.nerochat_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NerochatBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
